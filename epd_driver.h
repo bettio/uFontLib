1facb746e75173a0bc98c658dc43233e3056dae9
Davide Bettio <davide@uninstall.it>
2022-05-20 01:27:04 +0200

    Move epd_internals.h structs to epd_driver.h
    epd_internals.h is not required anymore.

--- epd_driver.h
+++ epd_driver.h
@@ -9,7 +9,36 @@ extern "C" {
 #pragma once
 #include <stdbool.h>
 #include <stdint.h>
-#include "epd_internals.h"
+
+/// Font data stored PER GLYPH
+typedef struct __attribute__((__packed__)) {
+  uint16_t width;            ///< Bitmap dimensions in pixels
+  uint16_t height;           ///< Bitmap dimensions in pixels
+  uint16_t advance_x;        ///< Distance to advance cursor (x axis)
+  int16_t left;             ///< X dist from cursor pos to UL corner
+  int16_t top;              ///< Y dist from cursor pos to UL corner
+  uint32_t compressed_size; ///< Size of the zlib-compressed font data.
+  uint32_t data_offset;     ///< Pointer into EpdFont->bitmap
+} EpdGlyph;
+
+/// Glyph interval structure
+typedef struct __attribute__((__packed__))  {
+  uint32_t first;  ///< The first unicode code point of the interval
+  uint32_t last;   ///< The last unicode code point of the interval
+  uint32_t offset; ///< Index of the first code point into the glyph array
+} EpdUnicodeInterval;
+
+/// Data stored for FONT AS A WHOLE
+typedef struct {
+  const uint8_t *bitmap;            ///< Glyph bitmaps, concatenated
+  const EpdGlyph *glyph;            ///< Glyph array
+  const EpdUnicodeInterval *intervals; ///< Valid unicode intervals for this font
+  uint32_t interval_count;    ///< Number of unicode intervals.
+  bool compressed;            ///< Does this font use compressed glyph bitmaps?
+  uint16_t advance_y;         ///< Newline distance (y axis)
+  int ascender;               ///< Maximal height of a glyph above the base line
+  int descender;              ///< Maximal height of a glyph below the base line
+} EpdFont;
 
 /// An area on the display.
 typedef struct {
